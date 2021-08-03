ae3e6dc6ab35bda7bc042e1c6dd9f3b42c930833
Ahmed Dhouib <dhahmed2@gmail.com>
2021-08-03 17:47:19 +0100

    Sort on all columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Employee} from "./employee";
 import {ApicallService} from "./apicall.service";
+import {OrderPipe} from "ngx-order-pipe";
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,21 @@ export class AppComponent implements OnInit {
 
   pageEmployees: any;
   pageNumber : number = 1;
+  order : string = 'employee_name';
+  reverse: boolean = false;
+  sortedCollection: any[];
 
-  constructor(private apiService: ApicallService) {
+  constructor(private apiService: ApicallService,private orderPipe: OrderPipe) {
+    this.sortedCollection = orderPipe.transform(this.pageEmployees, 'employee_name');
+    console.log(this.sortedCollection);
+  }
+
+  setOrder(value: string) {
+    if (this.order === value) {
+      this.reverse = !this.reverse;
+    }
+
+    this.order = value;
   }
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule} from "ng2-search-filter";
 import { NgxPaginationModule} from "ngx-pagination";
+// import { Ng2OrderModule } from 'ng2-order-pipe';
+import { OrderModule } from 'ngx-order-pipe';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +15,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, Ng2SearchPipeModule, FormsModule, NgxPaginationModule
+    BrowserModule, HttpClientModule, Ng2SearchPipeModule, FormsModule, NgxPaginationModule, OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
